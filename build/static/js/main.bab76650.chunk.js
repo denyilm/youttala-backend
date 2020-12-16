48299b08db2991b89ce4068a65ea7e7b7005e74f
unknown <yilmaz.deniz.itu@gmail.com>
2020-12-16 08:59:32 +0100

    161220-dy-1
    new build with window.addEventListener

--- build/static/js/main.bab76650.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpyouttala-frontend"]=this["webpackJsonpyouttala-frontend"]||[]).push([[0],{30:function(n,t,e){},62:function(n,t,e){"use strict";e.r(t);var o=e(0),s=(e(1),e(20)),c=e.n(s);e(30),e(3),e(7),e(4),e(8);e(21);var a=e(5),i=(e(63),function(){return Object(o.jsx)("div",{className:"launch",children:Object(o.jsxs)("span",{id:"coming-soon",children:["coming soon! ",Object(o.jsx)(a.g,{size:23})]})})});c.a.render(Object(o.jsx)(i,{}),document.getElementById("root"))}},[[62,1,2]]]);
-//# sourceMappingURL=main.bab76650.chunk.js.map
