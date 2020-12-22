e4e5aab60e86a229aacfa0fb9dfadd0121f37147
unknown <yilmaz.deniz.itu@gmail.com>
2020-12-22 18:20:57 +0100

    221220-dy-2
    redirect-ssl package-trial-2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 ﻿/* eslint-disable no-unused-vars */
 const config = require('./utils/config')
+//added 221220
 var sslRedirect = require('heroku-ssl-redirect')
 const express = require('express')
 //requires downloading the package
 require('express-async-errors')
-//added 221220
 const app = express()
 const cors = require('cors')
 const subtitlesRouter = require('./controllers/subtitles')
@@ -25,10 +25,12 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+//added 221220
+app.use(sslRedirect())
 app.use('/api/subtitles', subtitlesRouter)
 
 app.use(middleware.unknownEndPoint)
 app.use(middleware.errorHandler)
-app.use(sslRedirect())
+
 
 module.exports = app
